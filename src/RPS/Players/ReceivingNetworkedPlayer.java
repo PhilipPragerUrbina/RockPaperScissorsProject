bc030891831da7d5ee5e3a4c54d88176a351c14e
PhilipPragerUrbina <72355251+PhilipPragerUrbina@users.noreply.github.com>
2022-10-03 16:38:01 -0700

    First networked prototype

--- src/RPS/Players/ReceivingNetworkedPlayer.java
+++ src/RPS/Players/ReceivingNetworkedPlayer.java
@@ -1,34 +1,88 @@
 package RPS.Players;
 
 import RPS.Player;
+import RPS.RPS;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.Socket;
-import java.util.logging.SocketHandler;
 
-//get  moves over the network
+//get moves from a SendingNetworkPlayer over the network
+//The client side
+//acts like a puppet for the actual player on another computer
+
+/* Data interchange protocol:
+    - Sever sends action(int)           S
+    - Client sends result 1(int)        C
+    - Server confirmation(void)         S
+    - Client sends result 2(int)        C
+    - Server confirmation(void)         S
+    - Client sends result 3(int)        C
+    Repeat.                             S
+ */
+
+
 public class ReceivingNetworkedPlayer implements Player {
-    private Socket socket;
-    private DataOutputStream input;
+
+    private Socket socket; //the socket to connect to
+    private DataOutputStream data_to_send; //outgoing data
+    private DataInputStream incoming_data; //input data
+
+    //Create a player that will get moves from an address and port
     public ReceivingNetworkedPlayer(String address, int port){
         try {
+            Thread.sleep(50); //wait for server to start up(in case client starts up first)     todo: make scanning loop
+            //create socket and steam
             socket = new Socket(address,port);
-            input = new DataOutputStream(socket.getOutputStream());
+            data_to_send = new DataOutputStream(socket.getOutputStream());
+            incoming_data = new DataInputStream(socket.getInputStream());
+            System.out.println("Getting data from: " + socket.getInetAddress().getHostName());
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        System.out.println("Connected to other player");
     }
+
+    //get the next move over the network and respond with confirmation
     @Override
     public int getMove() {
-        return ;
+        try {
+            int move = incoming_data.readInt(); //get move
+            //no need for confirmation, will send back round data next
+            System.out.println("Move received: " + RPS.intToString(move));
+            return move;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return RPS.ROCK; //return rock if not receiving anything
+        }
     }
 
     @Override
     public void saveLastRoundData(int yourMove, int opponentMove, int outcome) {
-        //nothing
+        //send data back to player, so it can make decisions if necessary
+        try {//always have a response for every write, and write for every send.
+            //This is so the connection is reliable
+        data_to_send.writeInt(yourMove);  //write your-move
+        incoming_data.read(); //await confirmation
+        data_to_send.writeInt(opponentMove); //write opponent move
+        incoming_data.read();  //await confirmation
+        data_to_send.writeInt(outcome); //write outcome
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //clean up(not necessary if not connecting to anything else)
+    public void close(){
+        try {
+            data_to_send.close();
+            incoming_data.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ;
     }
 }
---
+++ src/RPS/Players/SendingNetworkedPlayer.java
@@ -0,0 +1,95 @@
+package RPS.Players;
+
+import RPS.Player;
+import RPS.RPS;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//send moves from Player to a ReceivingNetworkPlayer over the network
+//The Server side
+
+/* Data interchange protocol:
+    - Sever sends action(int)           S
+    - Client sends result 1(int)        C
+    - Server confirmation(void)         S
+    - Client sends result 2(int)        C
+    - Server confirmation(void)         S
+    - Client sends result 3(int)        C
+    Repeat.                             S
+ */
+
+
+public class SendingNetworkedPlayer implements Player {
+
+    private Socket socket; //the socket to connect to
+    private ServerSocket server; //the server to await connection
+    private DataOutputStream data_to_send; //outgoing data
+    private DataInputStream incoming_data; //input data
+    private Player controlling_player;
+
+    //Create a layer that will send moves from a player over the network
+    public SendingNetworkedPlayer( Player player,int port){
+        try {
+            //await connection
+            server = new ServerSocket(port);
+            System.out.println("Server awaiting connection");
+            socket = server.accept();
+            System.out.println("Sending data to:" + socket.getInetAddress().getHostName());
+            //create streams
+            data_to_send = new DataOutputStream(socket.getOutputStream());
+            incoming_data = new DataInputStream(socket.getInputStream()); //todo add buffered input stream
+            System.out.println("Connected to: " + socket.getInetAddress().getHostName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //get the next move of the player, and send it over the network
+    @Override
+    public int getMove() {
+        int move = controlling_player.getMove(); //get local player move
+        try {
+            //send move
+            data_to_send.writeInt(move);
+            System.out.println("Move sent: "  + RPS.intToString(move));
+            return move;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return move; //return local player move
+    }
+
+    //receive data and pipe to underlying player
+    @Override
+    public void saveLastRoundData(int yourMove, int opponentMove, int outcome) {
+        try {
+            //get data
+            yourMove = incoming_data.readInt(); //read your move
+            data_to_send.write(0); //write confirmation
+            opponentMove = incoming_data.readInt(); //read opponent move
+            data_to_send.write(0); //write confirmation
+            outcome = incoming_data.readInt(); //read outcome
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        controlling_player.saveLastRoundData(yourMove,opponentMove,outcome); //save data to underlying player(can fall back on local data)
+
+    }
+
+    //clean up(not necessary if not connecting to anything else)
+    public void close(){
+        try {
+            incoming_data.close();
+            data_to_send.close();;
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ;
+    }
+}
